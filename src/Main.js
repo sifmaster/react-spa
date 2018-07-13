8bcdfc2de9b27113240efe32e8172d4995b549ce
djurango <djuric@me.com>
2018-07-13 17:23:41 +0200

    Version 1.1

--- src/Main.js
+++ src/Main.js
@@ -18,17 +18,22 @@ class Main extends Component {
                         <h1>React Boilerplate by Djurango</h1>
                     </header>
 
-
-                    <ul className="nav">
+                     <ul className="nav">
                         <li><NavLink exact to="/">Home</NavLink></li>
                         <li><NavLink to="/stuff">Stuff</NavLink></li>
                         <li><NavLink to="/contact">Contact</NavLink></li>
                     </ul>
+
                     <div className="content">
                         <Route exact path="/" component={Home}/>
                         <Route path="/stuff" component={Stuff}/>
                         <Route path="/contact" component={Contact}/>
                     </div>
+
+                    <footer>
+                        <p>(c) djurango</p>
+                    </footer>
+
                 </div>
             </HashRouter>
         );
